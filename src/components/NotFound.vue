3b69b4db58c44109c10359aea8269bdf1742baf8
Gantelia <helengriva@gmail.com>
2023-05-06 14:47:10 +0300

    Добавит компонент NotFound

---
+++ src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="not-found">
+    <p>404<br />Страница не найдена</p>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  font-size: 1.2rem;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Charts from '../components/Charts.vue'
 import Table from '../components/Table.vue'
+import NotFound from '../components/NotFound.vue'
 
 const routes = [
   { name: 'Charts', path: '/', component: Charts },
-  { name: 'Table', path: '/table', component: Table }
+  { name: 'Table', path: '/table', component: Table },
+  { name: 'NotFound', path: '/:pathMatch(.*)*', component: NotFound }
 ]
 
 export const vueRouter = createRouter({
